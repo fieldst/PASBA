01f4ee6aeebe668ac042cec724d1371dcdacb842
fieldst <tfieldsvender@hotmail.com>
2016-10-08 16:40:35 -0500

    update with pasba apps

---
+++ pasbahome/js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('pasbaapp', [
+	'ngRoute',
+	'angularSmoothscroll',
+	'ui.calendar',
+	'ui.bootstrap',
+	]);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when("/home", {
+		templateUrl: "views/pasbahome.html",
+		controller: 'pasbacontroller',
+	}).when("/pasbaapps", {
+		templateUrl: "views/pasbaapps.html",
+		controller: 'pasbacontroller',
+		css: 'css/pasbaappscss.css',
+
+	}).
+	otherwise({
+	  redirectTo: "/home"
+	});
+
+
+	});
+
+
+
+
+
+
+
+
+
