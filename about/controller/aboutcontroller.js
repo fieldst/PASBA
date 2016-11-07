bf0d024b64d6882fffa2d8f63449403149a27d9a
Fields II <1279155234@mil>
2016-11-07 10:48:26 -0600

    fix

---
+++ about/controller/aboutcontroller.js
@@ -0,0 +1,32 @@
+app.controller('aboutuscontroller', function($scope, $compile, $timeout) {
+  // $scope.introimage = 'dmrpc/images/dmrpc-intro.svg',
+  $scope.pageimage = {
+
+
+              directorimage : "content/images/braxton.jpg",
+              logo : "content/images/PASBA-new-logo1.png",
+              dataquality : "about/images/dqlight.svg",
+              coding: "about/images/codinglight.svg",
+              development: "about/images/developmentlight.svg",
+              dataanalysis: "about/images/dataanalysislight.svg",
+              datainput: "about/images/datainputlight.svg",
+
+                                        }
+
+        $scope.title = {
+                                                leadership : "Leadership",
+                                                aboutus : "About Us",
+                                                pad : "PAD Deployed Medical Documentation",
+                                                regulate : "Medical Regulating",
+                                                sidr : "Instructions for proccessing deployed SIDR"
+        }
+
+        // $scope.documents = {
+        //                                               recordsrequest : "dmrpc/documents/AuthorizationforDisclosureofMedicalorDentalInformation(DD2870).pdf"
+        // }
+ 
+                
+
+ 
+
+});
---
+++ shared/controllers/appcontroller.js
@@ -0,0 +1,14 @@
+app.controller('pasbacontroller', function($scope, $compile, $timeout, uiCalendarConfig) {
+  $scope.test = 'test';
+
+   $scope.apps = [
+  
+        {name: 'MAP'}
+
+  ]
+
+});
+
+
+
+
